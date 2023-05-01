327035d94341ef1ed9289a0b60841f0b4ca0cb2c
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-05-01 12:16:15 +0530

    feat: add withdraw logic with shamir secrets

--- src/SMixer.sol
+++ src/SMixer.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.8.13;
 
 contract SMixer {
     error InvalidValueSize();
+    error InvalidSecret();
+    error TransferFailed();
 
     event Deposit(address depositer, uint256 value);
 
     mapping(bytes32 => uint256) internal withdrawHashMapping;
+
+    struct Secrets {
+        uint256 x;
+        uint256 y;
+    }
 
     modifier checkSendSize() {
         if (
@@ -20,5 +27,25 @@ contract SMixer {
     function deposit(bytes32 _hash) external payable checkSendSize {
         withdrawHashMapping[_hash] = msg.value;
         emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(Secrets[] memory secrets) external {
+        uint256 secretKey = _recoverSecret(secrets);
+        bytes32 _hash = keccak256(abi.encodePacked(msg.sender, secretKey));
+        uint256 amount = withdrawHashMapping[_hash];
+        if (amount == 0) revert InvalidSecret();
+        (bool s, ) = address(msg.sender).call{value: amount}("");
+        if (!s) revert TransferFailed();
+    }
+
+    function _recoverSecret(
+        Secrets[] memory _secrets
+    ) internal pure returns (uint256 secret) {
+        int256 l0 = int(_secrets[0].y * _secrets[1].x) /
+            int(_secrets[1].x - _secrets[0].x);
+        int256 l1 = int(_secrets[1].y * _secrets[0].x) /
+            int(_secrets[0].x - _secrets[1].x);
+
+        return uint256(l0 + l1);
     }
 }
